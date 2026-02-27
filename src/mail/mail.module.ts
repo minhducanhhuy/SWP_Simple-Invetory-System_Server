46f876f2a0b2b845594df7c785ce17ff47f9760b
minhducanhhuy <minhducanhhuy@gmail.com>
2026-02-27 11:44:43 +0700

    chỉnh sửa lại module mail

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -2,11 +2,28 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { join } from 'path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
   imports: [
-    // Cấu hình MailerModule.forRoot ở đây hoặc ở AppModule đều được
-    // Nhưng thường MailModule sẽ setup MailerModule
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: Number(process.env.MAIL_PORT),
+        secure: false,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      },
+      defaults: { from: process.env.MAIL_FROM },
+      template: {
+        dir: join(process.cwd(), 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: { strict: true },
+      },
+    }),
   ],
   providers: [MailService],
   exports: [MailService], // <--- BẮT BUỘC PHẢI CÓ DÒNG NÀY
