20b1fb726afa816f075784176fc162c9489f011f
minhducanhhuy <minhducanhhuy@gmail.com>
2026-03-24 13:20:25 +0700

    thay đổi mật khẩu

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,26 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
+
+export class ChangePasswordDto {
+  @IsNotEmpty({ message: 'Vui lòng nhập mật khẩu hiện tại' })
+  @IsString()
+  oldPassword: string;
+
+  @IsNotEmpty({ message: 'Vui lòng nhập mật khẩu mới' })
+  @IsString()
+  @MinLength(6, { message: 'Mật khẩu phải có ít nhất 6 ký tự' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Mật khẩu phải có ít nhất 1 chữ hoa, 1 chữ thường, 1 số và 1 ký tự đặc biệt',
+  })
+  newPassword: string;
+}
